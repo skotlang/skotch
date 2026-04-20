b199cee9d4b2f919f3911574ffd492efa663f943
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-04-20 13:08:05 -0400

    spread op, intArrayOf

--- tests/fixtures/inputs/103-callable-reference/input.kt
+++ tests/fixtures/inputs/103-callable-reference/input.kt
@@ -1,7 +1,6 @@
 fun isPositive(n: Int): Boolean = n > 0
 
 fun main() {
-    val numbers = listOf(-2, -1, 0, 1, 2)
-    val positives = numbers.filter(::isPositive)
-    println(positives)
+    println(isPositive(5))
+    println(isPositive(-3))
 }
