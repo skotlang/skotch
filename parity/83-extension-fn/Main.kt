57d55bc64b0ee24c67722f65054b77bcbf619658
Marc Prud hommeaux <mwp1@cornell.edu>
2026-06-27 18:00:33 -0400

    parity: 83-extension-fn — top-level extension fn on String + Int

---
+++ parity/83-extension-fn/Main.kt
@@ -0,0 +1,12 @@
+fun String.reverseWords(): String = this.split(" ").reversed().joinToString(" ")
+fun Int.times(s: String): String {
+    val sb = StringBuilder()
+    for (i in 0 until this) sb.append(s)
+    return sb.toString()
+}
+
+fun main() {
+    println("hello world from kotlin".reverseWords())
+    println(3.times("ab"))
+    println(0.times("x"))
+}
