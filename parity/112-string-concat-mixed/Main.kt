2a726608a3d3876932ea7805a7f013aa27639a13
Marc Prud hommeaux <mwp1@cornell.edu>
2026-06-27 23:53:29 -0400

    parity: 112-string-concat-mixed — string + non-String operands

---
+++ parity/112-string-concat-mixed/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val a = "x"
+    val b = 7
+    val c = 3.14
+    val d = true
+    println(a + b)
+    println(a + c)
+    println(a + d)
+    println("" + 1 + 2)
+    println("" + 1.0 + 2.0)
+    println("[" + b + "," + c + "]")
+}
