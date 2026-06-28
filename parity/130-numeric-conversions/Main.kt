abbb808479c641fcfa4d42acac210351f49e5682
Marc Prud hommeaux <mwp1@cornell.edu>
2026-06-28 01:52:45 -0400

    parity: 130-numeric-conversions — Int/Long/Double/Float conversions

---
+++ parity/130-numeric-conversions/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val i = 7
+    val l = i.toLong()
+    val d = i.toDouble()
+    val f = i.toFloat()
+    val s = i.toString()
+    println(l)
+    println(d)
+    println(f)
+    println(s)
+
+    val d2 = 3.14
+    println(d2.toInt())
+    println(d2.toLong())
+
+    val str = "42"
+    println(str.toInt())
+    println(str.toLong())
+}
