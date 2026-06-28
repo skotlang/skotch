814de5d4c36d6df6291cbed003929df944da540d
Marc Prud hommeaux <mwp1@cornell.edu>
2026-06-27 23:53:33 -0400

    parity: 115-unary-ops — unary +/-/! / ++ / --

---
+++ parity/115-unary-ops/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val x = 7
+    val y = -x
+    val z = +x
+    println(y)
+    println(z)
+    val b = true
+    val nb = !b
+    println(nb)
+    println(!nb)
+    var a = 5
+    a++
+    println(a)
+    a--
+    println(a)
+    println(++a)
+    println(--a)
+}
