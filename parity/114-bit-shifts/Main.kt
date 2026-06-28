cbe01a82cdc587513229f884883ba2d58133ccfe
Marc Prud hommeaux <mwp1@cornell.edu>
2026-06-27 23:53:32 -0400

    parity: 114-bit-shifts — shl/shr/ushr/and/or/xor/inv bit ops

---
+++ parity/114-bit-shifts/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val n = 12
+    println(n shl 2)
+    println(n shr 1)
+    println(n ushr 1)
+    println(n and 7)
+    println(n or 1)
+    println(n xor 6)
+    println(n.inv())
+}
