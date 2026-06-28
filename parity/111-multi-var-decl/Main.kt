faa4f78c9bf420b58142d9b5b8fd822dfbba0eea
Marc Prud hommeaux <mwp1@cornell.edu>
2026-06-27 23:53:27 -0400

    parity: 111-multi-var-decl — multi var-decl with deferred assignment

---
+++ parity/111-multi-var-decl/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val a: Int; val b: Int; val c: Int
+    a = 1
+    b = 2
+    c = 3
+    println(a + b + c)
+    var x = 10
+    var y = 20
+    val tmp = x
+    x = y
+    y = tmp
+    println("$x $y")
+}
