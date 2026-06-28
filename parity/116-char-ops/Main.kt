f079eda551a2ad46fd0257c6035235b9eb947a96
Marc Prud hommeaux <mwp1@cornell.edu>
2026-06-27 23:53:35 -0400

    parity: 116-char-ops — Char code/+/lowercase/isLetter/isDigit/digitToInt

---
+++ parity/116-char-ops/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val c = 'A'
+    println(c)
+    println(c.code)
+    println((c + 1))
+    println(c.lowercaseChar())
+    println(c.isLetter())
+    println(c.isDigit())
+    val d = '5'
+    println(d.isDigit())
+    println(d.digitToInt())
+}
